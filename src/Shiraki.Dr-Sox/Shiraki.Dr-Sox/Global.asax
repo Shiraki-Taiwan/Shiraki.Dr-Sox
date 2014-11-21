032555313518c5f428e95f3eee80c9822f9f7f8e
Chen, Po Hung <eric.chen@walkermedia.com.tw>
2014-11-21 19:07:47 +0800

    2014-11-21 Update (1)

---
+++ src/Shiraki.Dr-Sox/Shiraki.Dr-Sox/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Shiraki.Dr_Sox.MvcApplication" Language="C#" %>
---
+++ src/Shiraki.Dr-Sox/Shiraki.Dr-Sox/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Shiraki.Dr_Sox.Startup))]
+namespace Shiraki.Dr_Sox
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
